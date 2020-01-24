88c142b1e744e0faabb8480c87769be661493302
Clifton <Jarrett_Clifton@steris.com>
2020-01-24 09:58:58 -0500

    Fixed Outer Join and limited to 'order' header type

--- Joining.sql
+++ Joining.sql
@@ -1,33 +1,44 @@
-SELECT DISTINCT ottt.transaction_type_ID         
+SELECT DISTINCT ottt.TRANSACTION_TYPE_ID
 ,               ottt.NAME
 ,               ottt.DESCRIPTION
+,               hou.NAME AS "Operating Unit"
 ,               CASE
-                    WHEN xp.parameter_type = 'ORDER_TYPE_HIST'
+                    WHEN xp.PARAMETER_TYPE       = 'ORDER_TYPE_HIST'
                     THEN 'Y'
                     ELSE 'N'
                 END AS "Included On Store"
-FROM xxecomm_parameters         xp
-,    oe_transaction_types_all   otta
-,    oe_transaction_types_tl    ottt
+FROM XXECOMM_PARAMETERS         xp
+,    OE_TRANSACTION_TYPES_ALL   otta
+,    OE_TRANSACTION_TYPES_TL    ottt
+,    HR_OPERATING_UNITS         hou
 WHERE 1 = 1
-AND otta.transaction_type_id    = ottt.transaction_type_id
-AND ottt.language               = 'US'
-AND (otta.end_date_active       IS NULL                     OR otta.end_date_active > SYSDATE)
-AND (otta.org_id                = 82                        OR otta.org_id = 83)
-ORDER BY ottt.transaction_type_ID;
+AND xp.PARAMETER_VALUE_NUM  (+) = otta.TRANSACTION_TYPE_ID
+AND otta.TRANSACTION_TYPE_ID    = ottt.TRANSACTION_TYPE_ID
+AND otta.ORG_ID                 = hou.ORGANIZATION_ID
+AND otta.TRANSACTION_TYPE_CODE  = 'ORDER'
+AND ottt.LANGUAGE               = 'US'
+AND (otta.END_DATE_ACTIVE       IS NULL          OR otta.END_DATE_ACTIVE > SYSDATE)
+AND (otta.ORG_ID                = 82             OR otta.ORG_ID = 83)
+ORDER BY ottt.TRANSACTION_TYPE_ID;
+
+SELECT otta.TRANSACTION_TYPE_ID                  -- Failed OUTER JOIN, correct syntax?
+FROM OE_TRANSACTION_TYPES_ALL otta
+LEFT OUTER JOIN XXECOMM_PARAMETERS
+ON otta.TRANSACTION_TYPE_ID     = XXECOMM_PARAMETERS.PARAMTER_VALUE_NUM;
 
 
-SELECT DISTINCT otta.org_id                      -- Finding org id's
-,               ottt.name
-FROM oe_transaction_types_all   otta
-,    oe_transaction_types_tl    ottt
-WHERE ottt.transaction_type_id  = otta.transaction_type_id;
+SELECT DISTINCT otta.ORG_ID                      -- Finding org id's
+,               ottt.NAME
+FROM OE_TRANSACTION_TYPES_ALL   otta
+,    OE_TRANSACTION_TYPES_TL    ottt
+WHERE ottt.TRANSACTION_TYPE_ID  = otta.TRANSACTION_TYPE_ID;
 
 
 SELECT *                                         -- order types contained on webstore
 FROM XXECOMM_PARAMETERS
-WHERE parameter_type            = 'ORDER_TYPE_HIST';
+WHERE PARAMETER_TYPE            = 'ORDER_TYPE_HIST';
 
 
-oe_transaction_types_all;                        -- describing tables
-oe_transaction_types_tl;
+OE_TRANSACTION_TYPES_ALL;                        -- describing tables
+OE_TRANSACTION_TYPES_TL;
+HR_OPERATING_UNITS;
